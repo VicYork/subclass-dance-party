c41009d0140ebf1398c379b23dd832063407b36e
Victor Carter <Victor.A.York@gmail.com>
2015-10-13 20:03:05 -0500

    fixed everything but linup.

---
+++ src/dalaiLama.js
@@ -0,0 +1,16 @@
+/**
+ * Created by VicYork on 13/10/15.
+ */
+var DalaiLama = function (top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.attr("id", "dalaiLama");
+  this.$node.append(('<img src="assets/dalai-lama-disobey.png">'));
+};
+
+DalaiLama.prototype = Object.create(Dancer.prototype);
+DalaiLama.prototype.constructor = DalaiLama;
+
+DalaiLama.prototype.step = function () {
+  Dancer.prototype.step.call(this);
+  this.$node.toggle();
+};
---
+++ src/pitbull.js
@@ -0,0 +1,19 @@
+var Pitbull = function(top, left, timeBetweenSteps){
+	Dancer.call(this, top, left, timeBetweenSteps);
+this.$node.attr("id", "pitbull");
+};
+
+Pitbull.prototype = Object.create(Dancer.prototype);
+Pitbull.prototype.constructor = Pitbull;
+
+Pitbull.prototype.step = function(){
+	Dancer.prototype.step.call(this);
+	var grow = function(){
+		this.$node.animate({
+		width: 32px;
+		height: 32px;
+	},1000, grow();)
+
+	};
+
+};
