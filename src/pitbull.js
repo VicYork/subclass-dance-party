d20caebc14f2009347ee662adf33e5bf7e061142
Victor Carter <Victor.A.York@gmail.com>
2015-10-13 20:17:18 -0500

    Pitbull working. Working on Dalai Lama spin.

--- src/pitbull.js
+++ src/pitbull.js
@@ -1,19 +1,18 @@
-var Pitbull = function(top, left, timeBetweenSteps){
-	Dancer.call(this, top, left, timeBetweenSteps);
-this.$node.attr("id", "pitbull");
+var Pitbull = function (top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.attr("id", "pitbull");
 };
 
 Pitbull.prototype = Object.create(Dancer.prototype);
 Pitbull.prototype.constructor = Pitbull;
 
-Pitbull.prototype.step = function(){
-	Dancer.prototype.step.call(this);
-	//var grow = function(){
-	//	this.$node.animate({
-	//	width: 32px;
-	//	height: 32px;
-	//},1000, grow();)
-  //
-	//};
+Pitbull.prototype.step = function () {
+  Dancer.prototype.step.call(this);
 
+  this.$node.animate({
+    //top : "",
+    //bottom: "",
+    left: "toggle"
+  }, 5000, function () {
+  });
 };
